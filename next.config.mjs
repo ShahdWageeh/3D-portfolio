674e05e0ca6d9ff67f37c0f51d30e50632c7f4d9
ShahdWageeh <shahdwageeh79@gmail.com>
2025-04-07 15:25:43 +0200

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,2 @@
-// import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
-    images: {
-        unoptimized: true,
-    },
-    trailingSlash: true,
-};
+const nextConfig = {};
